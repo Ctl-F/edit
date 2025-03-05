524d26351aa082c97ed5ab1d04228653e18b0901
Spencer Brough <broughspencer@gmail.com>
2025-03-05 08:38:30 -0700

    textbuffer

---
+++ include/textbuffer.h
@@ -0,0 +1,19 @@
+#ifndef __TEXTBUFFER_H__
+#define __TEXTBUFFER_H__
+
+namespace ed {
+
+class TextBuffer {
+public:
+	TextBuffer();
+	TextBuffer(const char* initial_text);
+	~TextBuffer();
+
+private:
+
+};
+
+}
+
+#endif
+
