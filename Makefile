21bfefbdcd502ca5bb1f7c4be14289bfbc8a1901
Spencer Brough <broughspencer@gmail.com>
2025-03-06 21:01:56 -0700

    Textbuffer

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+
+edit:
+	g++ -o ./bin/edit ./src/main.cpp -I./include/ -lSDL3 -lSDL_image -lSDL_ttf
+
+
--- src/textbuffer.cpp
+++ src/textbuffer.cpp
@@ -1,4 +1,29 @@
 #include "textbuffer.h"
 
+#include <cstring>
 
+TextBuffer::TextBuffer() : m_BufferBegin{nullptr}, m_BufferEnd{nullptr} {
+	m_BufferBegin = new TextNode{};
+	m_BufferEnd = m_BufferBegin;
+	m_CursorNode = m_BufferBegin;
+}
+TextBuffer::TextBuffer(const char* initial_text){
+	m_BufferBegin = new TextNode{};
+	m_BufferEnd = m_BufferBegin;
+	m_CursorNode = m_BufferBegin;
+
+	insert(initial_text);
+}
+TextBuffer::~TextBuffer(){
+	TextNode *where = m_BufferBegin;
+	while(where->next != nullptr){
+		where = where->next;
+		delete where->previous;
+	}
+	delete where;
+}
+
+void TextBuffer::insert(const char* text){
+	
+}
  
